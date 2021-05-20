cac4958ba641e98aad8b7a5e7efb40263526650b
Lukas Kurz <vb.net@kurzweb.de>
2021-05-20 09:06:32 +0200

    Moved hwnd interops and added some more interops and a helper class

---
+++ CoreWindowInterop/IInspectable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UWPInterop
+{
+
+    [ComImport]
+    [Guid("AF86E2E0-B12D-4c6a-9C5A-D7AA65101E90")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IInspectable
+    {
+        void GetIids(out int iidCount, [MarshalAs(UnmanagedType.LPArray)] out Guid[] iids);
+        void GetRuntimeClassName(out IntPtr className);
+        void GetTrustLevel(out IntPtr trustLevel);
+    }
+
+}
---
+++ CoreWindowInterop/InteropHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UWPInterop
+{
+
+    public class InteropHelper
+    {
+        public static Guid GetIID<T>()
+        {
+            return GetIID(typeof(T));
+        }
+
+        public static Guid GetIID<T>(string interfaceName)
+        {
+            return GetIID(typeof(T).GetInterface(interfaceName));
+        }
+
+        public static Guid GetIID(Type type)
+        {
+            return type.GUID;
+        }
+    }
+
+}
