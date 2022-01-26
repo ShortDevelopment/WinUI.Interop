4c0efdc190bf4f7fd515dfc803fc9ed78770f824
Lukas Kurz <lukas@shortdev.de>
2022-01-26 22:14:46 +0100

    Added more functionality for WinUI Windows

--- WinUI.Interop/NativeWindow/WindowExtensions.Icon.cs
+++ WinUI.Interop/NativeWindow/WindowExtensions.Icon.cs
@@ -11,10 +11,6 @@ using System.Runtime.InteropServices;
 
 namespace WinUI.Interop.NativeWindow
 {
-    /// <summary>
-    /// Provides Extension for the <c>WinUI</c> <see cref="Microsoft.UI.Xaml.Window"/> <br/>
-    /// Can be used with any <c>Win32</c> Window
-    /// </summary>
     public static partial class WindowExtensions
     {
         /// <summary>
--- WinUI.Interop/NativeWindow/WindowExtensions.cs
+++ WinUI.Interop/NativeWindow/WindowExtensions.cs
@@ -1,9 +1,38 @@
-﻿
+﻿#if USING_WINUI
+using Microsoft.UI.Xaml;
+using WinRT;
+#endif
+
+using System;
+using Microsoft.UI.Windowing;
+using Microsoft.UI;
+
 namespace WinUI.Interop.NativeWindow
 {
     /// <summary>
     /// Provides Extension for the <c>WinUI</c> <see cref="Microsoft.UI.Xaml.Window"/> <br/>
     /// Can be used with any <c>Win32</c> Window
     /// </summary>
-    public static partial class WindowExtensions { }
+    public static partial class WindowExtensions
+    {
+#if USING_WINUI
+        /// <summary>
+        /// Get's the handle of a <c>WinUI</c> <see cref="Microsoft.UI.Xaml.Window"/>
+        /// </summary>
+        /// <param name="window">Reference to a <c>WinUI</c> <see cref="Microsoft.UI.Xaml.Window"/></param>
+        public static IntPtr GetHandle(this Window window) => window.As<IWindowNative>().WindowHandle;
+
+        /// <summary>
+        /// Get's the <see cref="AppWindow"/> that is assigned to the <c>WinUI</c> <see cref="Window"/>
+        /// </summary>
+        /// <param name="window">Reference to a <c>WinUI</c> <see cref="Microsoft.UI.Xaml.Window"/></param>
+        /// <returns></returns>
+        public static AppWindow GetAppWindow(this Window window)
+        {
+            IntPtr hWnd = window.GetHandle();
+            var winId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(winId);
+        }
+#endif
+    }
 }
