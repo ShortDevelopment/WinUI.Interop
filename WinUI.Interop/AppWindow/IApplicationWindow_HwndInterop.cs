a43b6129f29ee6b7997cd676ab346b27ec8d273c
Lukas Kurz <lukas@shortdev.de>
2022-12-18 15:11:23 +0100

    `IApplicationWindow_HwndInterop`

---
+++ WinUI.Interop/AppWindow/IApplicationWindow_HwndInterop.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WinUI.Interop.AppWindow;
+
+[Guid("b74ea3bc-43c1-521f-9c75-e5c15054d78c")]
+[InterfaceType(ComInterfaceType.InterfaceIsIInspectable)]
+interface IApplicationWindow_HwndInterop
+{
+    IntPtr WindowHandle { get; }
+}
---
+++ WinUI.Interop/AppWindow/UwpAppWindowExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UwpAppWindow = Windows.UI.WindowManagement.AppWindow;
+
+namespace WinUI.Interop.AppWindow;
+
+public static class UwpAppWindowExtensions
+{
+    public static IntPtr GetHwnd(this UwpAppWindow window)
+            => ((IApplicationWindow_HwndInterop)(object)window).WindowHandle;
+}
